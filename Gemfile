5500899ad4d336c04fd150ab6f557da359e9487a
Anthony Bargnesi <abargnesi@gmail.com>
2014-05-09 09:38:33 -0400

    json spec

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'json'
+gem 'json-schema'
+
+group :development do
+  gem 'rspec', '~> 2.14'
+end
+# vim: ts=2 sw=2
---
+++ schema.rb
@@ -0,0 +1,14 @@
+require 'json'
+require 'json-schema'
+
+schema_errors = JSON::Validator.fully_validate_schema('schema.json')
+unless schema_errors.empty?
+  puts "Schema is invalid."
+  schema_errors.each {|err| puts err}
+  exit 1
+end
+
+json_data = (STDIN.tty?) ? File.read(ARGV[0]) : STDIN.read
+if json_data
+  puts JSON::Validator.validate_json('schema.json', json_data)
+end
